db128afdab0f57ceff16a6eb17d764bdcf6b926f
Andrew C. Dvorak <acdvorak@gmail.com>
2026-02-20 21:10:44 -0600

    Update `build.ps1` to compile both x86 and x64 binaries

--- build.ps1
+++ build.ps1
@@ -1,2 +1,11 @@
-cmake --build ./build -DCMAKE_BUILD_TYPE=Debug
-cmake --build ./build --config Debug
+$ErrorActionPreference = "Stop"
+
+$Configuration = "Debug"
+$Platforms = @("Win32", "x64")
+
+foreach ($Platform in $Platforms) {
+	$BuildDir = Join-Path $PSScriptRoot "build/$Platform"
+
+	cmake -S $PSScriptRoot -B $BuildDir -A $Platform
+	cmake --build $BuildDir --config $Configuration
+}
