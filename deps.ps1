04a04500baaa93befa1d8969d731ffd9d28935fc
Andrew C. Dvorak <acdvorak@gmail.com>
2026-02-21 22:41:25 -0600

    Formatting

--- deps.ps1
+++ deps.ps1
@@ -11,18 +11,25 @@ if (-not (Test-Path $installAdminScript)) {
 	throw "Missing script: '$installAdminScript'"
 }
 
+Write-Host ""
 Write-Host "Running install_user.ps1..."
+Write-Host ""
 & $installUserScript
 if ($LASTEXITCODE -ne 0) {
+  Write-Host ""
 	throw "install_user.ps1 failed with exit code $LASTEXITCODE."
 }
+Write-Host ""
 
 Write-Host ""
 Write-Host "Running install_admin.ps1..."
+Write-Host ""
 & $installAdminScript
 if ($LASTEXITCODE -ne 0) {
 	throw "install_admin.ps1 failed with exit code $LASTEXITCODE."
 }
+Write-Host ""
 
 Write-Host ""
 Write-Host "Dependency installation completed successfully."
+Write-Host ""
